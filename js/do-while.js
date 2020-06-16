fe876180a79381160eb6284e7d201df17acc4547
sukhanova <verusia.s@gmail.com>
2020-06-16 18:36:04 -0500

    Loops challenges added

--- js/do-while.js
+++ js/do-while.js
@@ -11,3 +11,20 @@ function getRandomNumber(upper) {
     console.log(`The random number is ${getRandomNumber(10)}`);
     counter ++;
   } while (counter < 20);
+
+
+  /*Challenge Task:
+  You don't always need to use a counter or specify an exact number of times that a loop must run. All you need is a condition that evaluates to false at some point so that the loop can end.
+
+The code in script.js opens a prompt dialog that asks for a password and assigns it to the variable secret. It also displays an alert dialog. Currently, no code checks the password.
+
+Add a do...while loop that keeps displaying the prompt dialog until the user types 'sesame'.
+  */
+
+// Display the prompt dialogue while the value assigned to `secret` is not equal to "sesame"
+let secret;
+do{
+  secret = prompt("What is the secret password?");
+} while (secret !=='sesame')
+// This should run after the loop is done executing
+alert("You know the secret password. Welcome!");
--- js/while.js
+++ js/while.js
@@ -10,3 +10,15 @@ while (counter < 10) {
   console.log(`The random number is ${getRandomNumber(10)}`);
   counter ++;
 }
+
+
+/* Challenge Task:
+Create a while loop that logs the current value of count to the console 26 times. Use the count variable to track the number of times the loop runs. Don't forget to increment count by one each time through the loop.
+*/
+
+let count = 0;
+
+while(count < 26 ){
+  console.log(count);
+  count ++
+}
