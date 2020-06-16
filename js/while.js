9944184547560f0ebbf366d83935b758d3ef7a0c
sukhanova <verusia.s@gmail.com>
2020-06-16 17:30:52 -0500

    The while loop

---
+++ js/while.js
@@ -0,0 +1,11 @@
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
+
+/* If you do nt know how many times the code should run, you can use a while loop */
+
+let counter = 0;
+while (counter < 10) {
+  console.log(`The random number is ${getRandomNumber(10)}`);
+  counter ++;
+}
