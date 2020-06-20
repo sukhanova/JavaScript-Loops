a8982b20881afba6d0667ea7e6d645a92d3bcc5d
sukhanova <verusia.s@gmail.com>
2020-06-20 15:25:02 -0500

    Added solution for tracking guess attempts

--- Practice-JavaScript-Loops/js/trackGuessAttempts.js
+++ Practice-JavaScript-Loops/js/trackGuessAttempts.js
@@ -2,6 +2,10 @@ const main = document.querySelector('main');
 const randomNumber = getRandomNumber(10);
 let guess;
 
+// Keep track of the number of guess attempts
+
+let attempts = 0;
+
 function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
@@ -9,7 +13,9 @@ function getRandomNumber(upper) {
 
 do {
   guess = prompt('Please guess a number between 1 and 10.');
+  attempts++;
 } while (parseInt(guess) !== randomNumber);
 
-main.innerHTML = `<h1>You guessed the number! It was ${randomNumber}.</h1>`;
+
+main.innerHTML = `<h1>You guessed the number! It took you ${attempts} tries to guess number ${randomNumber}.</h1>`;
 
